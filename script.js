1299093b58dbbeed4ff9ac07fff8564600d82b58
saideepakraj22 <saideepakraj22@gmail.com>
2026-03-13 21:36:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,43 @@ photo.style.display = "none";
 });
 
 });
+// Like Button
+
+document.querySelectorAll(".like-btn").forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+btn.classList.toggle("liked");
+
+if(btn.classList.contains("liked")){
+btn.innerHTML="💖";
+}else{
+btn.innerHTML="❤️";
+}
+
+});
+
+});
+
+
+// Lightbox Photo Viewer
+
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+
+img.addEventListener("click", () => {
+
+lightbox.style.display="flex";
+lightboxImg.src=img.src;
+
+});
+
+});
+
+closeBtn.onclick = () => {
+lightbox.style.display="none";
+}
