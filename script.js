0ddf81f3db1be30c266000d98abbbfae37e732c5
saideepakraj22 <saideepakraj22@gmail.com>
2026-03-15 15:39:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Instagram-style Like Button
 function likePhoto(btn){
     let countSpan = btn.querySelector("span");
     
@@ -31,70 +32,34 @@ function formatLikes(num){
     else return (num/1000000).toFixed(1).replace('.0','') + 'M';
 }
 
-    // Store count in data attribute so we always know the raw number
-    countSpan.dataset.count = count;
-
-    // Format number like Instagram
-    countSpan.innerText = formatLikes(count);
-
-    // Animate heart
-    btn.classList.add("liked");
-    setTimeout(()=> btn.classList.remove("liked"), 500);
-}
-
-// Format numbers like Instagram: 1K, 1.2M
-function formatLikes(num){
-    if(num < 1000) return num;
-    else if(num < 1000000) return (num/1000).toFixed(1).replace('.0','') + 'K';
-    else return (num/1000000).toFixed(1).replace('.0','') + 'M';
-}
-
+// Image Viewer
 function openViewer(img){
-
-document.getElementById("imageViewer").style.display="flex"
-
-document.getElementById("viewerImg").src = img.src
-
+    document.getElementById("imageViewer").style.display="flex";
+    document.getElementById("viewerImg").src = img.src;
 }
 
 function closeViewer(){
-
-document.getElementById("imageViewer").style.display="none"
-
+    document.getElementById("imageViewer").style.display="none";
 }
 
+// Disable right-click
 document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+    alert("Right click is disabled on this website");
+});
 
-e.preventDefault()
-
-alert("Right click is disabled on this website")
-
-})
-
+// Filter Photos
 function filterPhotos(category){
-
-let photos = document.querySelectorAll(".photo-card")
-
-photos.forEach(function(photo){
-
-if(category === "all"){
-
-photo.style.display = "block"
-
-}
-
-else if(photo.dataset.category === category){
-
-photo.style.display = "block"
-
-}
-
-else{
-
-photo.style.display = "none"
-
-}
-
-})
-
+    let photos = document.querySelectorAll(".photo-card");
+    photos.forEach(function(photo){
+        if(category === "all"){
+            photo.style.display="block";
+        }
+        else if(photo.dataset.category === category){
+            photo.style.display="block";
+        }
+        else{
+            photo.style.display="none";
+        }
+    });
 }
